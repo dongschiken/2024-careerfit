b3d0f014965bdf71974e6e6d5324c07209fbc3c9
dongs <don0421865@gmail.com>
2024-11-13 08:59:30 +0900

    2024-11-13 09:00 app.vue 수정

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,8 @@
 <template>
   <div>
     <router-view />
-<<<<<<< HEAD
     <!-- <button @click="increment()">증가</button>
     <div>{{ store.count }}</div> -->
-=======
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink :to="{ name: 'board' }">Board</RouterLink>
-      <RouterLink :to-="{ name : 'join' }">Join</RouterLink>
-    </nav>
->>>>>>> a8efb3e35bd89558cf6856d730a3cf7cbaa7970c
   </div>
 </template>
 
