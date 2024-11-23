ba2dcfc2657a1a979e8e0ed7bcb9c13965527382
dongs <138917930+dongschiken@users.noreply.github.com>
2024-11-23 15:53:22 +0900

    2024-11-23 16:00 chat 수정

--- backend/src/main/java/com/peach/careerfit/chat/controller/ChatRoomController.java
+++ backend/src/main/java/com/peach/careerfit/chat/controller/ChatRoomController.java
@@ -83,7 +83,6 @@ public class ChatRoomController {
 	@PostMapping("/chat-room/{chat_room_id}/users")
 	public ResponseEntity<String> joinChatRoom(
 	    @PathVariable("chat_room_id") int chatRoomId,
-	    @RequestBody ChatRoomUserRequest chatRoomUserRequest,
 	    HttpServletRequest request) {
 		ResponseTokenUser user = jwtResponse.extractTokenUser(request);
 	    if (user == null) {
@@ -146,12 +145,10 @@ public class ChatRoomController {
 			// ChatRoomService에서 데이터를 조회
 			System.out.println(chatRoomId);
 			ChatRoom chatRoom = chatRoomService.getChatRoomById(chatRoomId);
-
 			// 채팅방이 존재하지 않는 경우 처리
 			if (chatRoom == null) {
 				return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 			}
-
 			// 성공적으로 채팅방 정보를 반환
 			return ResponseEntity.ok(chatRoom);
 		} catch (Exception e) {
